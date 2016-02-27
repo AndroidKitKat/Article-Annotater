3c62fb2c91189f24bdf6112901de22f97603c470
Laury Lafage <lowrey97x@hotmail.fr>
2016-02-27 23:05:59 +0100

    Input Comment :-)

--- src/main/resources/static/js/angular-component/comment/angular-comment-directive.js
+++ src/main/resources/static/js/angular-component/comment/angular-comment-directive.js
@@ -5,15 +5,50 @@
 	  });
 	  app.directive('comment', function() {
 	      var controller = function($scope) {
-				var commentController = this;
-				$scope.open = false; // This will be binded using the ps-open attribute
-				commentController.toggle = function() {
-					$scope.open = !$scope.open
-				}
+			//var commentController = this;
+	    	var imagePath = 'https://material.angularjs.org/latest/img/list/60.jpeg?0';
+		    $scope.comments = [
+		      {
+		        face : imagePath,
+		        what: 'Brunch this weekend?',
+		        who: 'Min Li Chan',
+		        when: '3:08PM',
+		        notes: " Secondary line text Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam massa quam."
+		        		+ "Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum"
+		        		+ "velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos."
+		      },
+		      {
+		        face : imagePath,
+		        what: 'Brunch this weekend?',
+		        who: 'Min Li Chan',
+		        when: '3:08PM',
+		        notes: " I'll be in your neighborhood doing errands"
+		      }
+		    ];
+			$scope.open = false;
+			$scope.newComment = "";
+			$scope.toggle = function() {
+				$scope.open = !$scope.open;
+			}
 
-				$scope.$on('event:toggle', function(event, data) {
-					commentController.toggle();
-		    	});
+			$scope.addComment = function() {
+				var comment = {
+				        face : 'https://material.angularjs.org/latest/img/list/60.jpeg?0',
+				        what: 'Brunch this weekend?',
+				        who: 'Min Li Chan',
+				        when: '3:08PM',
+				        notes: $scope.newComment
+				      };
+				$scope.comments.push(comment);
+			}
+
+			$scope.isNewCommentEmpty = function() {
+				return $scope.newComment == null || $scope.newComment.length == 0;
+			};
+
+			$scope.$on('event:toggle', function(event, data) {
+				$scope.toggle();
+	    	});
 	      };
 	     
 	      return {
