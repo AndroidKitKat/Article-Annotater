a7828081878e5f55d74eda9517130c2a8d899975
Laury Lafage <lowrey97x@hotmail.fr>
2016-02-28 15:58:48 +0100

    Add contextual menu.

--- src/main/resources/static/js/angular-component/article/angular-article-directive.js
+++ src/main/resources/static/js/angular-component/article/angular-article-directive.js
@@ -1,22 +1,41 @@
 (function() {
-	  var app = angular.module('article-directive', []);
+	  var app = angular.module('article-directive', ['ui.bootstrap.contextMenu']);
 	  app.config(function($sceProvider) {
 		  // Completely disable SCE.  For demonstration purposes only!
 		  // Do not use in new projects.
 		  $sceProvider.enabled(false);
 	  });
-	  app.directive('article', function () {
-	      var controller = function () {
-	          var vm = this;
-	      };    
-	
-	      var link = function link(scope, element, attrs) {
-	    	  scope.toggleComments = function () {
+	app.directive('article', function () {
+		var controller = function () {
+			var vm = this;
+		};    
+
+		var getSelectionText = function() {
+			if(window.getSelection) {
+			    return window.getSelection().toString();
+			}
+				
+			if(document.selection && document.selection.type != "Control") {
+				return document.selection.createRange().text;
+			}
+
+			return "";
+		};
+		    
+		var link = function link(scope, element, attrs) {
+			scope.toggleComments = function () {
 				scope.$broadcast("event:toggle");
-	    	  }
-	      };
+			}
+
+			scope.menuOptions = [['Copy', function ($itemScope) {
+			}],
+			null, // Dividier
+			['Comment', function ($itemScope) {
+				scope.toggleComments();
+			}]];
+		};
 	     
-	      return {
+		return {
 	          restrict: 'EA', //Default for 1.3+
 	          scope: {
 	        	  text: '@text'
