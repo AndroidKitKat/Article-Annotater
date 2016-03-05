a0774e39426c7635f80dbed686a2ff0b23034718
Laury Lafage <lowrey97x@hotmail.fr>
2016-03-05 20:16:37 +0100

    Routing angular.js

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,17 +1,13 @@
  angular
-    	.module('ArticleAnnotater', ['ngMaterial', 'article-directive', 'comment-directive'])
-    	.controller('ExtractorCtrl', function($scope, $http, $sce) {
-			$scope.url = "";
-			$scope.article = "";
+    	.module('ArticleAnnotater', ['ngMaterial', 'ui.router', 'article-directive', 'comment-directive'])
+    	.config(function($stateProvider, $urlRouterProvider) {
+			$stateProvider
+			  .state('home', {
+			    url: '/',
+			    controller: 'ExtractorCtrl',
+			    templateUrl: 'extractor/extractor.html'
+			  });
 
-			$scope.extract = function() {
-				$http
-					.get('/article?url=' + encodeURIComponent($scope.url))
-			        .success(function(data) {
-			            $scope.article = $sce.trustAsHtml(data.text);
-			        });
-			};
-			$scope.isUrlEmpty = function() {
-				return $scope.url == null || $scope.url.length == 0;
-			};
-		});
+			  $urlRouterProvider.otherwise('/');;
+    	});
+		
