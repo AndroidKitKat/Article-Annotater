94bc45cf65e87910e6186e2037674fc84f74c61e
Laury Lafage <lowrey97x@hotmail.fr>
2016-03-08 20:45:21 +0100

    Add progress indicator.

--- src/main/resources/static/extractor/extractorController.js
+++ src/main/resources/static/extractor/extractorController.js
@@ -3,11 +3,14 @@
     	.controller('ExtractorCtrl', function($scope, $http, $sce) {
 			$scope.url = "";
 			$scope.article = "";
+			$scope.displayProgressBar = false;
 
 			$scope.extract = function() {
+				$scope.displayProgressBar = true;
 				$http
 					.get('/article?url=' + encodeURIComponent($scope.url))
 			        .success(function(data) {
+			        	$scope.displayProgressBar = false;
 			            $scope.article = $sce.trustAsHtml(data.text);
 			        });
 			};
